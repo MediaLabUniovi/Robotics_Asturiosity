0c8fdf2de4fced9f413b94bec96719eefe4c7d23
Alba19gf <alba.gonzzaalez@gmail.com>
2023-03-15 11:08:14 +0100

    read

--- src/main.cpp
+++ src/main.cpp
@@ -99,6 +99,24 @@ void calculateServoAngle()
 }
 
 void readDistance(){
+  digitalWrite(Trigger1, HIGH);
+  delayMicroseconds(10); // Enviamos un pulso de 10us
+  digitalWrite(Trigger1, LOW);
+
+  timeW1 = pulseIn(Echo1, HIGH); // obtenemos el ancho del pulso
+  distancia = timeW1 / 59;      // escalamos el tiempo a una distancia en cm
+
+  digitalWrite(Trigger1, HIGH);
+  delayMicroseconds(10); // Enviamos un pulso de 10us
+  digitalWrite(Trigger1, LOW);
+
+  // //* PRINTEO DE distanciaS
+  // Sensor motorW1
+  Serial.print("distancia motorW1: ");
+  Serial.print(distancia); // Enviamos serialmente el valor de la distancia
+  Serial.print("cm");
+  Serial.println();
+  delay(1000); // Hacemos una pausa de 100ms
   
 }
 // void readDistance()
