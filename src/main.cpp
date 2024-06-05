41f5361cba61cd7f54029e4642b561ab3a705501
Alba19gf <115873114+Alba19gf@users.noreply.github.com>
2024-06-05 21:17:19 +0200

    v

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,22 @@
 //*CODIGO MAESTRO
 
-
-//DECLARACIÓN DE LIBRERÍAS
+// DECLARACIÓN DE LIBRERÍAS
 #include <Arduino.h>
 #include <Wire.h>
+#include <twoWire.h>
 
-
-
+// Definir pines SDA y SCL
 #define SDA_PIN 21
 #define SCL_PIN 22
 
-void setup() {
-  // Iniciar comunicación I2C
- Wire.begin();
-  Serial.begin(115200); // Inicializa comunicación serial 
-  //delay(1000); //Esperar un poco
-
-
+void setup()
+{
+    // Iniciar comunicación I2C
+    Wire.begin(SDA_PIN, SCL_PIN);
+    Serial.begin(115200); // Inicializa comunicación serial
+                          // delay(1000); //Esperar un poco
 }
 
-void loop() {
-  
+void loop()
+{
 }
-
