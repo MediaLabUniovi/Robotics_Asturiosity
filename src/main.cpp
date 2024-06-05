1572f394144c8ed78c23219015ebabccd426edbc
Alba19gf <115873114+Alba19gf@users.noreply.github.com>
2024-06-05 21:15:18 +0200

    v

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,25 @@
+//*CODIGO MAESTRO
 
-//*DECLARACIÓN DE LIBRERÍAS
+
+//DECLARACIÓN DE LIBRERÍAS
 #include <Arduino.h>
+#include <Wire.h>
 
-#include <Servo.h>
-#include <ServoEasing.hpp> // proporciona una forma fácil de crear movimientos de servo suaves y personalizados.
-// Esta librería utiliza algoritmos de interpolación para calcular el movimiento de los servos de manera precisa y suave,
-// permitiendo a los usuarios crear patrones de movimiento específicos como ondas sinusoidales, ondas cuadradas, rampas, escalones, etc.
-// Además, la librería ofrece la capacidad de personalizar la curva de movimiento para adaptarse a diferentes requisitos.
-#include <IBusBM.h>
-// #include <AccelStepper.h>
-// #include <SoftwareSerial.h>
 
-//* DECLARACIÓN DE VARIABLES
-#define STOP_SIGNAL 38
 
-void setup()
-{
-    Serial.begin(115200);
-    pinMode(STOP_SIGNAL, INPUT);
-}
+#define SDA_PIN 21
+#define SCL_PIN 22
 
-void loop(){
-    int STOP = digitalRead(STOP_SIGNAL);
-    Serial.println(STOP);
-    delay(1000);
+void setup() {
+  // Iniciar comunicación I2C
+ Wire.begin();
+  Serial.begin(115200); // Inicializa comunicación serial 
+  //delay(1000); //Esperar un poco
+
 
 }
+
+void loop() {
+  
+}
+
